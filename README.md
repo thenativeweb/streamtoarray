1b9683e04398732e8e7211b57e09e7ed759594b2
Golo Roden <webmaster@goloroden.de>
2016-03-04 17:36:51 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # streamtoarray
-streamtoarray converts a stream to an array.
+
+streamtoarray converts streams to arrays.
+
+## Installation
+
+    $ npm install streamtoarray
+
+## Quick start
+
+First you need to integrate streamtoarray into your application.
+
+```javascript
+const toArray = require('streamtoarray');
+```
+
+Then you can call `toArray` with a stream. The stream gets converted and its contents are returned as an array once the stream has ended.
+
+```javascript
+toArray(stream, (err, array) => {
+  // ...
+});
+```
+
+If the stream emits an `error` event, the callback is handed over the error. Also, if the stream has already been closed, the callback is called with an error.
+
+## Running the build
+
+To build this module use [roboter](https://www.npmjs.com/package/roboter).
+
+    $ bot build-server
+
+## License
+
+The MIT License (MIT)
+Copyright (c) 2016 the native web.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ test/streamToArrayTests.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const stream = require('stream');
+
+const assert = require('assertthat');
+
+const streamToArray = require('../lib/streamToArray');
+
+const PassThrough = stream.PassThrough;
+
+const getClosingStream = function () {
+  const closingStream = new PassThrough({ objectMode: true });
+
+  process.nextTick(() => {
+    closingStream.end();
+  });
+
+  return closingStream;
+};
+
+const getClosedStream = function () {
+  const closedStream = new PassThrough({ objectMode: true });
+
+  closedStream.end();
+
+  return closedStream;
+};
+
+const getStream = function () {
+  const fakeStream = new PassThrough({ objectMode: true });
+
+  process.nextTick(() => {
+    fakeStream.write('foo');
+    fakeStream.write('bar');
+    fakeStream.end();
+  });
+
+  return fakeStream;
+};
+
+const getFailingStream = function () {
+  const failingStream = new PassThrough({ objectMode: true });
+
+  process.nextTick(() => {
+    failingStream.emit('error', new Error('some-error'));
+  });
+
+  return failingStream;
+};
+
+suite('streamToArray', function () {
+  test('is a function.', function (done) {
+    assert.that(streamToArray).is.ofType('function');
+    done();
+  });
+
+  test('throws an error if stream is missing.', function (done) {
+    assert.that(() => {
+      streamToArray();
+    }).is.throwing('Stream is missing.');
+    done();
+  });
+
+  test('throws an error if callback is missing.', function (done) {
+    assert.that(() => {
+      streamToArray(getClosingStream());
+    }).is.throwing('Callback is missing.');
+    done();
+  });
+
+  test('converts the stream to an array.', function (done) {
+    streamToArray(getStream(), (err, array) => {
+      assert.that(err).is.null();
+      assert.that(array).is.equalTo([ 'foo', 'bar' ]);
+      done();
+    });
+  });
+
+  test('handles stream errors.', function (done) {
+    streamToArray(getFailingStream(), err => {
+      assert.that(err).is.not.null();
+      assert.that(err.message).is.equalTo('some-error');
+      done();
+    });
+  });
+
+  test('returns an error if the stream is closed.', function (done) {
+    streamToArray(getClosedStream(), err => {
+      assert.that(err).is.not.null();
+      assert.that(err.message).is.equalTo('Stream is closed.');
+      done();
+    });
+  });
+});
