341c8f488e6ab27f03da4d0c6f10f55d95b5aa5e
Golo Roden <golo.roden@thenativeweb.io>
2018-02-08 20:17:01 +0100

    Release 1.0.0.

--- dist/streamToArray.js
+++ dist/streamToArray.js
@@ -1,42 +1,41 @@
 'use strict';
 
-var streamToArray = function streamToArray(stream, callback) {
-  if (!stream) {
-    throw new Error('Stream is missing.');
-  }
-  if (!callback) {
-    throw new Error('Callback is missing.');
-  }
+var streamToArray = function streamToArray(stream) {
+  return new Promise(function (resolve, reject) {
+    if (!stream) {
+      throw new Error('Stream is missing.');
+    }
 
-  var array = [];
+    var array = [];
 
-  var onData = void 0,
-      onEnd = void 0,
-      onError = void 0;
+    var onData = void 0,
+        onEnd = void 0,
+        onError = void 0;
 
-  var unsubscribe = function unsubscribe() {
-    stream.removeListener('data', onData);
-    stream.removeListener('error', onError);
-    stream.removeListener('end', onEnd);
-  };
+    var unsubscribe = function unsubscribe() {
+      stream.removeListener('data', onData);
+      stream.removeListener('error', onError);
+      stream.removeListener('end', onEnd);
+    };
 
-  onData = function onData(data) {
-    array.push(data);
-  };
+    onData = function onData(data) {
+      array.push(data);
+    };
 
-  onError = function onError(err) {
-    unsubscribe();
-    callback(err);
-  };
+    onError = function onError(err) {
+      unsubscribe();
+      reject(err);
+    };
 
-  onEnd = function onEnd() {
-    unsubscribe();
-    callback(null, array);
-  };
+    onEnd = function onEnd() {
+      unsubscribe();
+      resolve(array);
+    };
 
-  stream.on('data', onData);
-  stream.on('error', onError);
-  stream.on('end', onEnd);
+    stream.on('data', onData);
+    stream.on('error', onError);
+    stream.on('end', onEnd);
+  });
 };
 
 module.exports = streamToArray;
